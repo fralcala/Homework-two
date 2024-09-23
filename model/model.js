ecd07a0b20aae0a3ffc032392ec76a5ba1cbbe3f
fralcala <fralcala@iu.edu>
2024-09-22 23:48:07 -0400

    added to app and model js files as well as set up nav and structure scss flies

---
+++ model/model.js
@@ -0,0 +1,17 @@
+const homePage = `<div class="home">
+        <h1>Home</h1>
+        
+      </div>`;
+const productsPage = `<div class="products">
+        <h1>Products</h1>
+        
+      </div>`;
+
+const aboutPage = `<div class="about">
+        <h1>About</h1>
+        
+      </div>`;
+const contactPage = `<div class="contact">
+        <h1>Contact</h1>
+        
+      </div>`;
